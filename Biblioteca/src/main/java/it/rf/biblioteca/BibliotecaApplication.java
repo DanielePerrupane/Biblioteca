670c140b35ffa714c3f37c798c82d5048af7ffb4
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2025-04-23 11:02:10 +0200

    aggiusti

--- Biblioteca/src/main/java/it/rf/biblioteca/BibliotecaApplication.java
+++ Biblioteca/src/main/java/it/rf/biblioteca/BibliotecaApplication.java
@@ -1,4 +1,4 @@
-package it.rf.gestlido;
+package it.rf.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Biblioteca/src/main/java/it/rf/biblioteca/ServletInitializer.java
+++ Biblioteca/src/main/java/it/rf/biblioteca/ServletInitializer.java
@@ -1,4 +1,4 @@
-package it.rf.gestlido;
+package it.rf.biblioteca;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
