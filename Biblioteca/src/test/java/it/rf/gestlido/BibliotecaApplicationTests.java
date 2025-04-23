6958daa5c33d58371e50194b722d948d2def7b4c
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2025-04-23 10:55:51 +0200

    creazione progetto

---
+++ Biblioteca/src/test/java/it/rf/gestlido/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.rf.gestlido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
